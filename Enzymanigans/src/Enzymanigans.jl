59f8186e8b8b4870dba4b612a17dfaafb0126076
Valentin Churavy <v.churavy@gmail.com>
2023-11-13 17:25:01 -0500

    Make progress on ForwardMode

--- Enzymanigans/src/Enzymanigans.jl
+++ Enzymanigans/src/Enzymanigans.jl
@@ -1,9 +1,6 @@
 module Enzymanigans
 
-function autodiff(f, args...)
-    f(args...)
-end
-
+import EnzymeCore
 
 include("codecache.jl")
 const GLOBAL_CI_CACHE = CodeCache()
@@ -12,7 +9,7 @@ import Core: MethodMatch, MethodTable
 import Core.Compiler: _methods_by_ftype, InferenceParams, get_world_counter, MethodInstance,
     specialize_method, InferenceResult, typeinf, InferenceState, NativeInterpreter,
     code_cache, AbstractInterpreter, OptimizationParams, WorldView, MethodTableView, ArgInfo,
-    StmtInfo, singleton_type, argtypes_to_type, MethodMatchInfo, CallMeta
+    StmtInfo, singleton_type, argtypes_to_type, MethodMatchInfo, CallMeta, widenconst, anymap
 
 struct EnzymeInterpreter <: AbstractInterpreter
     global_cache::CodeCache
@@ -91,39 +88,116 @@ function whichtt(mtv::MethodTableView, sig)
     return match.method
 end
 
+function unwrap_annotation(@nospecialize(c))
+    if c isa Core.Compiler.Const
+        v = widenconst(c)
+        if v <: Type{<:EnzymeCore.Annotation}
+            v = v.parameters[1]
+            return Core.Compiler.Const(Type{eltype(v)})
+        end
+        if v <: EnzymeCore.Annotation 
+            return Core.Compiler.Const(eltype(v))
+        end
+    elseif widenconst(c) != c
+        error("Unhandled $c")
+    end
+    return c
+end
+    
 function Core.Compiler.abstract_call_gf_by_type(interp::EnzymeInterpreter, @nospecialize(f),
         arginfo::ArgInfo, si::StmtInfo, @nospecialize(atype), sv::InferenceState, max_methods::Int)
     (;argtypes) = arginfo
 
-    if f === autodiff
-        # Use Parent to infer rt
-        inner_argtypes = argtypes[2:end]
-        ft = inner_argtypes[1]
-        f = singleton_type(ft)
+    if f === EnzymeCore.autodiff && length(argtypes) >= 4
+        mode = argtypes[2]
+        ft_activity = argtypes[3]
+        maybe_rt_activity = argtypes[4]
+        @info "Found EnzymeCore.autodiff" mode ft_activity maybe_rt_activity
+        if widenconst(ft_activity)       <: EnzymeCore.Annotation &&
+           widenconst(maybe_rt_activity) <: Type{<:EnzymeCore.Annotation}
+            if widenconst(mode) <: EnzymeCore.ForwardMode
+                @info "Found EnzymeCore.autodiff with full information"
+                rt_activity = widenconst(maybe_rt_activity)
+                inner_argtypes = argtypes[5:end] # strip rt_activity from primal
+                pushfirst!(inner_argtypes, argtypes[3])
+                primal_argtypes = anymap(unwrap_annotation, inner_argtypes)
 
-        call = Core.Compiler.abstract_call_gf_by_type(interp.parent,
-                f, ArgInfo(nothing, inner_argtypes), StmtInfo(true), argtypes_to_type(inner_argtypes), sv, max_methods)
+                # 1. Infer primal
+                ft = primal_argtypes[1]
+                f = singleton_type(ft)
+                call = Core.Compiler.abstract_call_gf_by_type(interp.parent,
+                    f, ArgInfo(nothing, primal_argtypes), StmtInfo(true), argtypes_to_type(primal_argtypes), sv, max_methods)
 
-        # Now call Enzyme to generate OpaqueClosure
-        if isa(call.info, MethodMatchInfo)
-            if length(call.info.results.matches) == 0
-                @show inner_argtypes
-                error("")
+                # 2. Obtain MI
+                if isa(call.info, MethodMatchInfo)
+                    if length(call.info.results.matches) == 0
+                        @error "Found no matching call" primal_argtypes
+                        error("Enzyme compilation failed")
+                    end
+                    mi = specialize_method(only(call.info.results.matches), preexisting=true)
+                else
+                    @error "Unknown" call.info call
+                    error("")
+                end
+
+                # 4. Compile
+
+                # params = Enzyme.Compiler.EnzymeCompilerParams(
+                #     # Tuple{FA, TT.parameters...}, Mode, width, rt2, true, #=abiwrap=#true, ModifiedBetween, ReturnPrimal, ShadowInit, UnknownTapeType, ABI)
+                # job    = Enzyme.Compiler.CompilerJob(mi, CompilerConfig(target, params; kernel=false), interp.world)
+
+                # compile_result = Enzyme.Compiler.cached_compilation(job) # TODO this takes a lock. Safe from here?
+
+                # FMT = ForwardModeThunk{typeof(compile_result.adjoint), FA, rt2, Tuple{params.TT.parameters[2:end]...}, Val{width}, Val(ReturnPrimal)}
+                
+                # 3. Create an OpaqueClosure that contains the thunk statically and the enzymecall
+                # expr = quote
+                #     Base.@_inline_meta
+                #     thunk = $FMT($(compile_result.adjoint))
+                #     thunk(args...)
+                # end
+
+                # TODO: Cache
+                # ci = get(rinterp.unopt[rinterp.current_level], mi, nothing)
+                # clos = AbstractCompClosure(rinterp.current_level, 1, call.info, ci.stmt_info)
+                # TODO: Construct a OpaqueClosure that contains the abicall
+                # clos = Core.PartialOpaque(Core.OpaqueClosure{<:Tuple, <:Any}, nothing, sv.linfo, clos)
+
+                # TODO: Based on rt_activity and call.rt compute the return type of the OpaqueClosure
+                # return CallMeta(rt2, call.effects, call.info)
+            else
+                @error "Unkown mode" mode
             end
-            mi = specialize_method(call.info.results.matches[1], preexisting=true)
-            # TODO: Cache
-            # ci = get(rinterp.unopt[rinterp.current_level], mi, nothing)
-            # clos = AbstractCompClosure(rinterp.current_level, 1, call.info, ci.stmt_info)
-            # TODO: Construct a OpaqueClosure that contains the abicall
-            # clos = Core.PartialOpaque(Core.OpaqueClosure{<:Tuple, <:Any}, nothing, sv.linfo, clos)
-        else
-            @error "Unknown" inner_argtypes call.info call
-            error("")
+            # @show ft = inner_argtypes[2]
+            # @show inner_argtypes[3:end]
+            # # Use Parent to infer rt
+            # f = singleton_type(ft)
+            # # primal_argtypes 
+
+            # call = Core.Compiler.abstract_call_gf_by_type(interp.parent,
+            #         f, ArgInfo(nothing, inner_argtypes), StmtInfo(true), argtypes_to_type(inner_argtypes), sv, max_methods)
+
+            # # Now call Enzyme to generate OpaqueClosure
+            # if isa(call.info, MethodMatchInfo)
+            #     if length(call.info.results.matches) == 0
+            #         @show inner_argtypes
+            #         error("")
+            #     end
+            #     mi = specialize_method(call.info.results.matches[1], preexisting=true)
+            #     # TODO: Cache
+            #     # ci = get(rinterp.unopt[rinterp.current_level], mi, nothing)
+            #     # clos = AbstractCompClosure(rinterp.current_level, 1, call.info, ci.stmt_info)
+            #     # TODO: Construct a OpaqueClosure that contains the abicall
+            #     # clos = Core.PartialOpaque(Core.OpaqueClosure{<:Tuple, <:Any}, nothing, sv.linfo, clos)
+            # else
+            #     @error "Unknown" inner_argtypes call.info call
+            #     error("")
+            # end
+            # # TODO Obtain rt2
+            # # rt2 = call.rt
+            # rt2 = Float64
+            # return CallMeta(rt2, call.effects, call.info)
         end
-        # TODO Obtain rt2
-        # rt2 = call.rt
-        rt2 = Float64
-        return CallMeta(rt2, call.effects, call.info)
     end
 
     ret = @invoke Core.Compiler.abstract_call_gf_by_type(interp::AbstractInterpreter, f::Any,
